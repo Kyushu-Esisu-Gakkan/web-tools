4bff58ede10996c14f93ede4ad0bcf886749a210
Doggie <okubo@hakobune.co.jp>
2017-04-17 22:42:04 +0900

    Implement ‘hackpad’s ruby view’.

--- src/routes.php
+++ src/routes.php
@@ -8,3 +8,9 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/hackpad/ruby/{pad_id}', function ($request, $response, $args) {
+	return $this->view->render($response, 'treat_ruby.html', [
+		'pad_id' => $args['pad_id']
+	]);
+})->setName('treat_ruby');
